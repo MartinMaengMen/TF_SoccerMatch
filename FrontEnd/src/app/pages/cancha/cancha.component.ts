45194d0fc3c92164c71c2282a48da597bd15a638
Adrian <Adrian_Ames@outlook.com>
2019-06-30 01:37:23 -0500

    Frontend: Listar Canchas

--- FrontEnd/src/app/pages/cancha/cancha.component.ts
+++ FrontEnd/src/app/pages/cancha/cancha.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material'
+import { MatTableDataSource} from '@angular/material'
 import { Cancha } from 'src/app/_model/cancha';
 import { CanchaService } from 'src/app/_service/cancha.service';
 @Component({
@@ -9,8 +9,8 @@ import { CanchaService } from 'src/app/_service/cancha.service';
 })
 export class CanchaComponent implements OnInit {
 dataSource:MatTableDataSource<Cancha>
-displayedColumns=[]
-  constructor(private canchaService:CanchaService) { }
+displayedColumns=['idCancha','nombre','direccion','preciohora','propietario'];
+constructor(private canchaService:CanchaService) { }
 
   ngOnInit() {
     this.canchaService.listar().subscribe(data=>{this.dataSource=new MatTableDataSource(data);});
