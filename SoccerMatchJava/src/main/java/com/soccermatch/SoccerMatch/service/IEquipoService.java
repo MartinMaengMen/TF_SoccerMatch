7d20417897bc407e70eab7c808a188352e5af99a
Adrian <Adrian_Ames@outlook.com>
2019-06-29 22:41:45 -0500

    Backend: Eliminiación proyecto

--- SoccerMatchJava/src/main/java/com/soccermatch/SoccerMatch/service/IEquipoService.java
+++
@@ -1,7 +0,0 @@
-package com.soccermatch.SoccerMatch.service;
-
-import com.soccermatch.SoccerMatch.entity.Equipo;
-
-public interface IEquipoService extends CrudService<Equipo,Integer>{
-
-}
--- SoccerMatchJava/src/main/java/com/soccermatch/SoccerMatch/service/IParticipanteService.java
+++
@@ -1,7 +0,0 @@
-package com.soccermatch.SoccerMatch.service;
-
-import com.soccermatch.SoccerMatch.entity.Participante;
-
-public interface IParticipanteService extends CrudService<Participante,Integer>{
-
-}
